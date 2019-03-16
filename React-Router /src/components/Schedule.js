f580162a0951b0ad62b768d47cb1edf19a3c1a42
Bhumi saxena <41126620+bhumisaxena0223@users.noreply.github.com>
2019-03-16 07:07:48 +0530

    Add Files Via Upload

---
+++ React-Router /src/components/Schedule.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Schedule = () => (
+  <div>
+    <ul>
+      <li>5th Dec, 2018</li>
+      <li>1 Dec, 2018</li>
+      <li>Next 30th, nov</li>
+    </ul>
+  </div>
+)
+
+export default Schedule
